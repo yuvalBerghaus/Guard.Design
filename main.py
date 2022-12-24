e3ae407aa93c48ee378e4b02211a01348f7f788a
yuvalBerghaus <yuvalBerghaus>
2022-12-24 14:02:16 +0200

    cleaned more the code

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
 import base64
-
 from PIL import Image
 from flask import Flask, request
 import gzip
 from pymongo import MongoClient
 from io import BytesIO
-import gradio as gr
 from transformers import pipeline
 # Create a MongoClient to the MongoDB server
 client = MongoClient('mongodb+srv://guarddesign:HALXBHFFMvhm5kYb@cluster0.hblmqfd.mongodb.net/?retryWrites=true&w=majority')
@@ -18,12 +16,6 @@ pipe = pipeline("image-classification", "umm-maybe/AI-image-detector")
 @app.route('/')
 def index():
     return 'Hello, World!'
-
-@app.route('/lika', methods=['POST'])
-def rangaha():
-    data = request.json
-    print(data)
-    return 'hello'
 
 @app.route('/compress', methods=['POST'])
 def compress_it():
@@ -42,7 +34,7 @@ def compress_it():
     # Encode the compressed data in base64
     base64_data = base64.b64encode(compressed_data).decode()
     document = {'image_data' : base64_data,
-                'image_title' : user_name+'_'+image_title
+                'image_title' : user_name+'__'+image_title
                 }
     result = collection.insert_one(document)
     return str(result.inserted_id)
@@ -57,14 +49,13 @@ def decompress_it():
     data = request.json
     user_name = data['user_name']
     image_title = data['image_title']
-    document_title = user_name+'_'+image_title
-    document = {'image_title' : document_title}
+    id = user_name+'__'+image_title
+    document = {'image_title' : id}
     chosen_document = collection.find_one(document)
     # Decompress the data
     decompressed_data = gzip.decompress(base64.b64decode(chosen_document['image_data']))
     string_data = decompressed_data.decode()
     return string_data
-
 
 
 if __name__ == '__main__':
